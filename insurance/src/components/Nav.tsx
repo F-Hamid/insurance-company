9d644fed4148074dd6af1f47aa167340b6a16e38
F-Hamid <hamid.fateh81@gmail.com>
2023-02-11 17:10:09 +0100

    Navbar  setup

--- insurance/src/components/Nav.tsx
+++ insurance/src/components/Nav.tsx
@@ -1,5 +1,28 @@
+import logo from "../images/logo.png";
+
 const Nav = (): JSX.Element => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar w-full  absolute left-0 top-0 p-2  mx-0 flex justify-between">
+      <ul className="item-container center justify-start ml-16 ">
+        <li className="navItem mx-6 cursor-pointer text-xl font-semibold hover:bg-secondary py-3 px-4 rounded-md ease-in-out duration-500 ">
+          Item
+        </li>
+        <li className="navItem mx-6 cursor-pointer text-xl font-semibold  hover:bg-secondary py-3 px-4  rounded-md ease-in-out duration-500 ">
+          Item
+        </li>
+        <li className="navItem mx-6 cursor-pointer text-xl font-semibold  hover:bg-secondary py-3 px-4  rounded-md ease-in-out duration-500 ">
+          Item
+        </li>
+        <li className="navItem mx-6 cursor-pointer text-xl font-semibold  hover:bg-secondary py-3 px-4  rounded-md ease-in-out duration-500 ">
+          Item
+        </li>
+        <li className="navItem mx-6 cursor-pointer text-xl font-semibold  hover:bg-secondary py-3 px-4 rounded-md ease-in-out duration-500 ">
+          Item
+        </li>
+      </ul>
+      <img src={logo} className="logo w-20 h-20 mr-4" alt="company logo" />
+    </div>
+  );
 };
 
 export default Nav;
--- insurance/src/layouts/home.tsx
+++ insurance/src/layouts/home.tsx
@@ -1,10 +1,18 @@
 import NavBar from "../components/Nav";
+
+
+
 const Home = (): JSX.Element => {
   return (
-    <section className="w-screen h-screen center flex-row">
+    <section className="w-screen h-screen  flex-col relative">
       <NavBar />
 
-      <div className="header"></div>
+      <div className="main center w-full h-full ">
+        <div className="header w-1/2 h-full border-4">header</div>
+        <div className="photo w-1/2 h-full">
+          <img src="" alt="" />
+        </div>
+      </div>
     </section>
   );
 };
