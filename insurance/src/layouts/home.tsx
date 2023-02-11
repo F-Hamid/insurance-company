1422fb3f0c5dcb5e65d02573679eb4037d7dd1ef
F-Hamid <hamid.fateh81@gmail.com>
2023-02-11 17:25:27 +0100

    Navbar  setup

--- insurance/src/layouts/home.tsx
+++ insurance/src/layouts/home.tsx
@@ -1,16 +1,15 @@
 import NavBar from "../components/Nav";
-
-
+import blob from "../images/blob.png";
 
 const Home = (): JSX.Element => {
   return (
     <section className="w-screen h-screen  flex-col relative">
       <NavBar />
 
-      <div className="main center w-full h-full ">
-        <div className="header w-1/2 h-full border-4">header</div>
-        <div className="photo w-1/2 h-full">
-          <img src="" alt="" />
+      <div className="main center w-full h-full  ">
+        <div className="header w-1/2 h-full ">header</div>
+        <div className="photo w-1/2 h-full relative">
+          <img className="w-[38rem]  absolute top-56 left-56" src={blob} alt="blob" />
         </div>
       </div>
     </section>
