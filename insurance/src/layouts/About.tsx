3031af372cb27312869d29c01f17d71f2ba1abb0
F-Hamid <hamid.fateh81@gmail.com>
2023-02-15 20:25:38 +0100

    about done

--- insurance/src/layouts/About.tsx
+++ insurance/src/layouts/About.tsx
@@ -5,28 +5,28 @@ import about4 from "../images/about4.jpg";
 
 const About = () => {
   return (
-    <section className="about bg-bkg w-screen h-screen center flex-col justify-center relative">
+    <section className="about bg-bkg w-screen h-screen center flex-col justify-center relative overflow-hidden">
       <div className="rightBar absolute top-6 -right-72  w-9 h-[110rem] bg-primary  rotate-45 origin-top "></div>
       <WhyUs />
       <div className="main w-full h-full relative p-24 flex flex-row ">
         <div className="photos relative w-2/3 ">
           <img
             src={about1}
-            className="w-[24rem] absolute -top-10 left-2"
+            className="w-[24rem] absolute -top-10 left-2 rounded-sm "
             alt="smiling girl"
           />
           <img
             src={about4}
-            className="w-[20rem] h-2/3 absolute -bottom-24 left-36"
+            className="w-[20rem] h-2/3 absolute -bottom-24 left-32 rounded-sm"
             alt="worker"
           />
           <img
             src={about2}
-            className="w-[32rem] absolute top-6 right-20"
+            className="w-[34rem] absolute top-6 right-20 rounded-sm"
             alt="cocking girl"
           />
         </div>
-        <div className="text w-4/5 my-10 ">
+        <div className="text w-4/5 my-4 flex flex-col justify-start items-start ">
           <p className="w-3/4 text-2xl leading-10">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum id
             nesciunt, est cumque, tempore ex eveniet voluptate, ab nulla
@@ -37,6 +37,12 @@ const About = () => {
             consectetur voluptatum aspernatur. Sint modi eveniet similique earum
             ut aperiam quos illo, expedita incidunt nobis exercitationem.
           </p>
+          <button
+            type="button"
+            className="bg-secondary text-3xl text-text rounded-sm p-4 px-8 my-4  select-none hover:bg-cta focus:outline-none focus:shadow-outline"
+          >
+            More...
+          </button>
         </div>
       </div>
     </section>
